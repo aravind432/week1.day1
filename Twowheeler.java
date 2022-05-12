487e2e8a9be9a02dce588089ee19c0aa6653ce19
aravind432 <73532085+aravind432@users.noreply.github.com>
2022-05-11 22:02:01 -0700

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Twowheeler {
+	 int numberOfWheels=2;
+	 short numberOfMirrors=2;
+	 long chassisNumber=99626043210l;
+	 boolean isPunctured=true;
+	String bikename= "dio";
+	double runningKM=34566.56;
+
+	public static void main(String[] args) {
+		Twowheeler bik=new Twowheeler();
+		System.out.println(bik.numberOfWheels);
+		System.out.println(bik.numberOfMirrors);
+		System.out.println(bik.chassisNumber);
+		System.out.println(bik.isPunctured);
+		System.out.println(bik.bikename);
+		System.out.println(bik.runningKM);
+
+	}
+
+
+
+
+}
+
+
